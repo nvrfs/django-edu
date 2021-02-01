925dfc52f746dc0a973f6ac84ae1bd4379d8aa54
Бунёд <sfrvn@outlook.com>
2021-02-01 22:54:38 +0300

    Forms

--- django3/movies/forms.py
+++ django3/movies/forms.py
@@ -1,1 +1,11 @@
-# idk why
+from django import forms
+
+from .models import Review
+
+
+class ReviewForm(forms.ModelForm):
+    '''Форма отзывов'''
+
+    class Meta:
+        model = Review
+        fields = ('name', 'email', 'text')
--- django3/movies/views.py
+++ django3/movies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import DetailView, ListView
 
+from .forms import ReviewForm
 from .models import Movie
 
 
@@ -20,5 +21,14 @@ class MovieDetailView(DetailView):
 class AddReview(View):
     '''Оставить отзыв'''
     def post(self, request, pk):
-        print(request.POST)
-        return redirect('/')
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            # приостановка сохранения формы
+            form = form.save(commit=False)
+            if request.POST.get('parent'):
+                # parent_id in DB
+                form.parent_id = int(request.POST.get('parent'))
+            movie = Movie.objects.get(pk=pk)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
