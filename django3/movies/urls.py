70e130421d39b4f1161123c2577a910eda4d77c7
Бунёд <sfrvn@outlook.com>
2021-02-03 04:38:21 +0300

    Filtration

--- django3/movies/urls.py
+++ django3/movies/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from .views import ActorDetailView, AddReview, MovieDetailView, MoviesListView
+from .views import (
+    ActorDetailView,
+    AddReview,
+    JSONFilterMoviesView,
+    MoviesFilterView,
+    MovieDetailView,
+    MoviesListView
+)
 
 urlpatterns = [
     path('', MoviesListView.as_view(), name='movies_list'),
+    path('filter/', MoviesFilterView.as_view(), name='movies_filter'),
+    path('json-filter/', JSONFilterMoviesView.as_view(), name='json_filter'),
     path('<slug:slug>/', MovieDetailView.as_view(), name='movie_detail'),
     path('review/<int:pk>/', AddReview.as_view(), name='add_review'),
     path('actor/<str:slug>/', ActorDetailView.as_view(), name='actor_detail'),
 ]
-
--- django3/movies/views.py
+++ django3/movies/views.py
@@ -1,18 +1,30 @@
+from django.db.models import Q
+from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views import View
 from django.views.generic import DetailView, ListView
 
 from .forms import ReviewForm
-from .models import Actor, Category, Movie
+from .models import Actor, Category, Genre, Movie
 
 
-class MoviesListView(ListView):
+class GenreYear:
+    '''Жанры и годы выхода фильмов'''
+
+    def get_genres(self):
+        return Genre.objects.all()
+
+    def get_years(self):
+        return Movie.objects.filter(is_draft=False).order_by('year')
+
+
+class MoviesListView(GenreYear, ListView):
     '''Список фильмов'''
     model = Movie
     queryset = Movie.objects.filter(is_draft=False)
 
 
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     '''Полное описание фильма'''
     model = Movie
     slug_field = 'url'
@@ -34,8 +46,34 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-class ActorDetailView(DetailView):
+class ActorDetailView(GenreYear, DetailView):
     '''Информация об актёре / режиссёре'''
     model = Actor
     # template_name = 'movies/actor_detail.html'
     slug_field = 'name'
+
+
+class MoviesFilterView(GenreYear, ListView):
+    '''Фильтрация фильмов'''
+
+    def get_queryset(self):
+        return Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        )
+
+
+class JSONFilterMoviesView(ListView):
+    '''Фильтрация фильмов в json'''
+
+    def get_queryset(self):
+        return Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist('year')) |
+            Q(genres__in=self.request.GET.getlist('genre'))
+        ).distinct().values('title', 'tagline', 'url', 'poster')
+    
+    def get(self, request, *args, **kwargs):
+        return JsonResponse(
+            {'movies': list(self.get_queryset())},
+            safe=False,
+        )
